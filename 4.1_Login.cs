416bf75dc764f9fa06d83b744838940886363d15
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-21 00:58:14 +0800

    4.1 Documented

--- 4.1_Login.cs
+++ 4.1_Login.cs
@@ -14,16 +14,23 @@ namespace Session4
     //Initial Commit
     public partial class Login : Form
     {
+        //Initialise Competition Date
         DateTime endTime = new DateTime(2020, 07, 26, 9, 0, 0);
         public Login()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Triggered when Login button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void loginBtn_Click(object sender, EventArgs e)
         {
             using (var context = new Session4Entities())
             {
+                //Checks if User ID or Password field is empty
                 if (userIDBox.Text.Trim() == "" || passwordBox.Text.Trim() == "")
                 {
                     MessageBox.Show("Please check your login details!", "Empty Field(s)", MessageBoxButtons.OK,
@@ -34,11 +41,15 @@ namespace Session4
                     var getUser = (from x in context.Users
                                    where x.userId == userIDBox.Text
                                    select x).FirstOrDefault();
+
+                    //Check if User exist in DB
                     if (getUser == null)
                     {
                         MessageBox.Show("User does not exist!", "Invalid Login credentials",MessageBoxButtons.OK,
                         MessageBoxIcon.Error);
                     }
+
+                    //Check if password keyed in matches DB's password
                     else if (getUser.passwd != passwordBox.Text)
                     {
                         MessageBox.Show("Password is wrong!", "Invalid Login credentials", MessageBoxButtons.OK,
@@ -47,18 +58,24 @@ namespace Session4
                     else
                     {
                         MessageBox.Show($"Welcome {getUser.name}!", "Successful Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        //If user is Admin, direct to Admin Main Menu page - 4.2
                         if (getUser.User_Type.userTypeName == "Admin")
                         {
                             this.Hide();
                             (new AdminMain(getUser.userId)).ShowDialog();
                             this.Close();
                         }
+
+                        //If user is Expert, direct to Expert Main Menu page - 4.3
                         else if (getUser.User_Type.userTypeName == "Expert")
                         {
                             this.Hide();
                             (new ExpertMain(getUser.userId)).ShowDialog();
                             this.Close();
                         }
+
+                        //If user is Competitor, direct to Update Competitor's Record page - 4.6
                         else
                         {
                             this.Hide();
@@ -70,6 +87,11 @@ namespace Session4
             }
         }
 
+        /// <summary>
+        /// Triggered when the Choose button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void chooseBtn_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog()
@@ -79,12 +101,19 @@ namespace Session4
                 Title = "Import CSV data"
             };
             var i = openFileDialog.ShowDialog();
+
+            //If file selected, then store file name in textbox
             if (i == DialogResult.OK)
             {
                 textBox3.Text = openFileDialog.FileName;
             }
         }
 
+        /// <summary>
+        /// Onload, load the timer to Competition
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Login_Load(object sender, EventArgs e)
         {
 
@@ -96,6 +125,12 @@ namespace Session4
             t.Start(); ;
         }
 
+        /// <summary>
+        /// Reads the CSV from filepath from the textbox, then check if account exist. If it doesnm't,
+        /// add to DB
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uploadBtn_Click(object sender, EventArgs e)
         {
             string[] lines = File.ReadAllLines(textBox3.Text);
@@ -126,6 +161,11 @@ namespace Session4
             MessageBox.Show("Users added!", "Successful account creation(s)", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        /// <summary>
+        /// EventHandler for Timer ticking
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timer1_Tick(object sender, EventArgs e)
         {
             TimeSpan ts = endTime.Subtract(DateTime.Now);
